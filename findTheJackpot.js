0f6a2d4a42240947c62587e03e9bfffe215eb815
Payton <paytonl@uoregon.edu>
2022-04-23 06:34:32 -0400

    added the howManyToTheJackpot function

--- findTheJackpot.js
+++ findTheJackpot.js
@@ -26,4 +26,31 @@ const findTheJackpot = (numberOfAttempts, numberToBeat, charToMatch=undefined) =
     }
 }
 
-findTheJackpot(1000000000, 10);
+const howManyToTheJackpot = (numberToBeat, charToMatch=undefined) => {
+    let defaultChecker = charToMatch === undefined;
+    let numberToWin = 0;
+    let i = 1;
+    while (numberToWin < numberToBeat) {
+        let hexCode = getRandomHex(numberToBeat);
+        if (defaultChecker) {
+            charToMatch = hexCode[0];
+        }
+        for (let i = 0; i < numberToBeat; i++) {
+            if (hexCode[i] == charToMatch) {
+                numberToWin++;
+            }
+        }
+        if (numberToWin == numberToBeat) {
+            let attempts = i == 1 ? "attempt" : "attempts";
+            let formattedI = String(i).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+            console.log(`It took ${formattedI} ${attempts} to find this hexadecimal number: ${hexCode}`);
+        }
+        else {
+            numberToWin = 0;
+            i++;
+        }
+    }
+}
+
+findTheJackpot(1000, 3);
+howManyToTheJackpot(4, "f");
