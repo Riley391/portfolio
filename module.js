d30faa9bfa4b871b3200849efdd3785a46073a5c
Riley391 <82134179+Riley391@users.noreply.github.com>
2022-05-05 19:25:24 -0700

    Added more comments and a print function

--- module.js
+++ module.js
@@ -1,7 +1,12 @@
 module.exports = {
-    sumNums
+    sumNums,
+    print
 };
 
 function sumNums(num1, num2) {
     return num1 + num2;
+}
+
+function print(...args) {
+    console.log(...args);
 }
