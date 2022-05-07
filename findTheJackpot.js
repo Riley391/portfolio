e9f2ed019fccb5e476cd0e745f62ebcda6ee2e71
Riley391 <82134179+Riley391@users.noreply.github.com>
2022-05-07 12:09:08 -0700

    Started filling in comments on findTheJackpot.js

--- findTheJackpot.js
+++ findTheJackpot.js
@@ -1,5 +1,5 @@
 // I'm tired of getting confused between Python and JS so I imported my own print() function
-const { print } = require('./module');
+const { print } = require("./module");
 
 // Generates a hex-code of length specified in the parameter
 const getRandomHex = (length) => {
@@ -8,36 +8,48 @@ const getRandomHex = (length) => {
     // loop a number of times according to the length parameter
     for (let i = 0; i < length; i++) {
         // assigns a random hex-digit (including 0) to digit
-        let digit = (Math.random() * 16 | 0).toString(16);
+        let digit = ((Math.random() * 16) | 0).toString(16);
         // adds digit to the result string
         result += digit;
     }
     return result;
-}
+};
 
-// takes parameters int(numberOfAttempts)
-const findTheJackpot = (numberOfAttempts, numberToBeat, charToMatch=undefined) => {
-    let defaultChecker = charToMatch === undefined;
+/* takes parameters int(numberOfAttempts), int(numberToBeat), and string(charToMatch) and 
+attempts to find a randomly generated string with the specified number (numberToBeat) 
+of a single character (charToMatch) all within the specified number of attempts (numberOfAttempts) */
+const findTheJackpot = (numberOfAttempts, numberToBeat, charToMatch = undefined) => {
+    // check whether charToMatch was left unassigned and assign resulting boolean to defaultChecker
+    let defaultChecker = typeof charToMatch === undefined;
+    // array of matches which will eventually be returned
     let matches = [];
+    // loop this code as many times as specified by numberOfAttempts
     for (let i = 0; i < numberOfAttempts; i++) {
+        // get a random hexcode of length determined by numberToBeat and assign that value to hexCode
         let hexCode = getRandomHex(numberToBeat);
+        // if charToMatch was left as the default, assign the first character of the random code to it
         if (defaultChecker) {
             charToMatch = hexCode[0];
         }
+        // declare variable to track number of character matches
         let numberToWin = 0;
-        for (let i = 0; i < numberToBeat; i++) {
+        // loop through each character in hexCode
+        for (let i = 0; i < hexCode.length; i++) {
+            // If the character is a match, add 1 to numberToWin
             if (hexCode[i] == charToMatch) {
                 numberToWin++;
             }
         }
+        // Check to see if the threshold is met
         if (numberToWin == numberToBeat) {
+            // If so, add it to the matches array
             matches.push(hexCode);
         }
     }
     return matches;
-}
+};
 
-const howManyToTheJackpot = (numberToBeat, charToMatch=undefined) => {
+const howManyToTheJackpot = (numberToBeat, charToMatch = undefined) => {
     let defaultChecker = charToMatch === undefined;
     let numberToWin = 0;
     let i = 1;
@@ -53,15 +65,14 @@ const howManyToTheJackpot = (numberToBeat, charToMatch=undefined) => {
         }
         if (numberToWin == numberToBeat) {
             let attempts = i == 1 ? "attempt" : "attempts";
-            let formattedI = String(i).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+            let formattedI = String(i).replace(/(.)(?=(\d{3})+$)/g, "$1,");
             return `It took ${formattedI} ${attempts} to find this hexadecimal number: ${hexCode}`;
-        }
-        else {
+        } else {
             numberToWin = 0;
             i++;
         }
     }
-}
+};
 
 print(findTheJackpot(1000, 3));
 print(howManyToTheJackpot(5, "7"));
