c1b0b15342fffe216c07fda27f7b81830166c868
Riley391 <82134179+Riley391@users.noreply.github.com>
2022-05-25 16:43:31 -0700

    added requiredCharacters functionality and refactored for readability

--- wordle-helper/wordle.py
+++ wordle-helper/wordle.py
@@ -1,29 +1,34 @@
-# TODO: add disallowedCharacters functionality (work out the bugs)
+# TODO: add yellow letter (requiredCharacters) functionality with consideration for position
+# TODO: create console-based interface for the program
 
 import csv
-dictionary = []
 
-with open("./wordle-helper/wordle-dictionary.csv", newline='') as csvfile:
+def buildDictionary():
+    dictionary = []
+    with open("./wordle-helper/wordle-dictionary.csv", newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter='\t')
         for row in spamreader:
             dictionary.append(row)
+    for i in range(len(dictionary)):
+        dictionary[i] = dictionary[i][0].lower()
+    return dictionary
 
-for i in range(len(dictionary)):
-    dictionary[i] = dictionary[i][0]
+def buildTestWordComposition(testWord):
+    testWordComposition = []
+    for i in range(len(testWord)):
+        if testWord[i] == "*":
+            testWordComposition.append(i)
+    return testWordComposition
 
-for i in range(len(dictionary)):
-    dictionary[i] = dictionary[i].lower()
-
-""" for i in range(20):
-    print(dictionary[i]) """
-
-def WordleHelper(testWord, disallowedCharacters=[]):
-    # testSet = ["album", "alloy", "algae", "alley", "glass"]
-    resultSet = []
+def buildThreshold(testWord):
     threshold = 0
     for character in testWord:
         if character != "*":
             threshold += 1
+    return threshold
+
+def checkForSoftMatches(dictionary, testWord, threshold):
+    resultSet = []
     for word in dictionary:
         testThreshold = 0
         for i in range(5):
@@ -33,24 +38,54 @@ def WordleHelper(testWord, disallowedCharacters=[]):
                 testThreshold += 1
         if testThreshold == threshold:
             resultSet.append(word)
-    k = 0
-    while k < len(resultSet):
+    return resultSet
+
+def checkForDisallowedCharacters(resultSet, disallowedCharacters):
+    i = 0
+    while i < len(resultSet):
         j = 0
-        while j < len(resultSet[k]):
+        while j < len(resultSet[i]):
             for character in disallowedCharacters:
-                if resultSet[k][j] == character:
+                if resultSet[i][j] == character:
                     try:
-                        resultSet.remove(resultSet[k])
+                        resultSet.remove(resultSet[i])
                     finally:
-                        k = 0
+                        i = 0
                         j -= 1
                         break
             j += 1
-        k += 1
+        i += 1
     return resultSet
 
-def PrintWordleHelper(testWord, disallowedCharacters=[]):
-    for word in WordleHelper(testWord, disallowedCharacters):
+def checkForRequiredCharacters(resultSetNoDisallowedCharacters, requiredCharacters, testWordComposition):
+    resultSetChecked = []
+    m = 0
+    resultSetCheckedLength = 0
+    while m < len(resultSetNoDisallowedCharacters):
+        for aCharacter in requiredCharacters:
+            for number in testWordComposition:
+                if resultSetNoDisallowedCharacters[m][number] == aCharacter:
+                    resultSetChecked.append(resultSetNoDisallowedCharacters[m])
+                    break
+                resultSetCheckedLength = len(resultSetChecked)
+            if len(resultSetChecked) != resultSetCheckedLength:
+                break
+        m += 1
+    return resultSetChecked
+
+def wordleHelper(testWord, requiredCharacters=[], disallowedCharacters=[]):
+    dictionary = buildDictionary()
+    testWordComposition = buildTestWordComposition(testWord)
+    threshold = buildThreshold(testWord)
+    resultSet = checkForSoftMatches(dictionary, testWord, threshold)
+    resultSetNoDisallowedCharacters = checkForDisallowedCharacters(resultSet, disallowedCharacters)
+    resultSetChecked = checkForRequiredCharacters(resultSetNoDisallowedCharacters, requiredCharacters, testWordComposition)
+    if not resultSetChecked:
+        return resultSetNoDisallowedCharacters
+    return resultSetChecked
+
+def printWordleHelper(testWord, requiredCharacters=[], disallowedCharacters=[]):
+    for word in wordleHelper(testWord, requiredCharacters, disallowedCharacters):
         print(word)
 
-PrintWordleHelper("*las*", ['b', 'k', 'g', 'c', 'f'])
+printWordleHelper("cro**", [], ['a', 'n', 'e', 'u', 'p'])
